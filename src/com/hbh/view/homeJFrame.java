bc23e986b72fa1e59bfc0490582a1e0b545b7342
ADMIN <ADMIN@DESKTOP-03SHNOG>
2020-11-16 21:04:49 +0700

    init

--- src/com/hbh/view/homeJFrame.java
+++ src/com/hbh/view/homeJFrame.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package com.hbh;
+package com.hbh.view;
 
 import java.awt.CardLayout;
 import javax.swing.UIManager;
